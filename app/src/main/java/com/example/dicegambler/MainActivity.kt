b3849e0ce9d43dee8386502be61adf03359e1860
alexru <rualex2008@gmail.com>
2023-09-25 11:40:02 -0400

    Update formatting to match best practice
    Signed-off-by: alexru <rualex2008@gmail.com>

--- app/src/main/java/com/example/dicegambler/MainActivity.kt
+++ app/src/main/java/com/example/dicegambler/MainActivity.kt
@@ -7,8 +7,6 @@ import android.widget.TextView
 
 /**
  * Rolls two dice, compares the two, and displays them with score
- * @version 09.24.23
- * @author Alex Ru
  */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,19 +14,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // Finds UI elements
-        val dice1button: Button = findViewById(R.id.left_button)
-        val dice2button: Button = findViewById(R.id.right_button)
+        val dice1Button: Button = findViewById(R.id.left_button)
+        val dice2Button: Button = findViewById(R.id.right_button)
         val scoreText: TextView = findViewById(R.id.score)
 
         // Updates score and rolls dice based on user choice
         var score = 0
-        dice1button.setOnClickListener {
+        dice1Button.setOnClickListener {
             if (rollDice() == 0) {
                 score += 1
             }
             scoreText.text = score.toString()
         }
-        dice2button.setOnClickListener {
+        dice2Button.setOnClickListener {
             if (rollDice() == 2) {
                 score += 1
             }
